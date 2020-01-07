40c086f466578b8e42ce4fae2932fc942214258f
Yaoyuxiao010524 <https://github.com/Yaoyuxiao010524/clabs.git>
2020-01-07 00:02:13 -0800

    added a file

---
+++ ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main(){
+
+    double a;
+
+    double b;
+
+    double c;
+
+    printf("please input two numbers(divided by space)\n");
+
+    scanf("%lf %lf",&a,&b);
+
+    c=(a+b)*(a+b);
+
+    printf("%.2lf\n",c);
+
+}
